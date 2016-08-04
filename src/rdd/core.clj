d1c39e24d4e22ba96d0d33fc5dd7b07baba31343
Tap <visibletrap@gmail.com>
2016-08-04 22:36:01 +0700

    EP 1

--- src/rdd/core.clj
+++ src/rdd/core.clj
@@ -1,9 +1,6 @@
 (ns rdd.core
   (:require [hickory.core :refer [parse as-hickory]]
-            [hickory.select :as hs]
-            [aleph.http :as aleph]
-            [ring.middleware.json :refer [wrap-json-response]]
-            [ring.util.response :refer [response]]))
+            [hickory.select :as hs]))
 
 (defn prettify-raw [record]
   (->> record
@@ -16,10 +13,11 @@
           [rank (first content) contrib public-repos public-gits location href hireable]))
        (zipmap [:rank :username :contribs :public-repos :public-gits :location :picture :hireable])))
 
-(defn peeps [raw-data]
+(defn chiang-mai-peeps [raw-data]
   (->> raw-data
        (rest)
-       (map prettify-raw)))
+       (map prettify-raw)
+       (filter (fn [{:keys [location]}] (re-seq #"Chiang" location)))))
 
 (defn fetch-and-parse []
   (->> (slurp "https://gist.github.com/statguy/6489c06f2425c8836f2243ff01542c6b")
@@ -27,28 +25,4 @@
        (as-hickory)
        (#(hs/select (hs/tag :tr) %))))
 
-(defonce D (atom nil))
-
-(defn init-data []
-  (reset! D (peeps (fetch-and-parse))))
-
-(def handler
-  (wrap-json-response
-    (fn [{:keys [uri]}]
-      (case (#{"/" "/api"} uri)
-        "/api" (response @D)
-        "/" (response "<html><body><h1>HELLO</h1></body></html>")
-        (response "nothing")))))
-
-(defonce S (atom nil))
-
-(defn start! []
-  (reset! S (aleph/start-server handler {:port 8000})))
-
-(defn stop! []
-  (when-let [s @S] (.close s))
-  (reset! S nil))
-
-(defn restart! []
-  (stop!)
-  (start!))
+(comment (chiang-mai-peeps (fetch-and-parse)))
